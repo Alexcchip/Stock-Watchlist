a37ad28c7ff6c0cbdac316f410b6a573cc0e54bc
Alex W <alexcchip@gmail.com>
2023-06-04 17:27:56 -0400

    First commit

---
+++ main.py
@@ -0,0 +1,9 @@
+print("     Welcome to your personalized stock watchlist")
+while True:
+    intial = int(input("""Read: 1)
+    Edit: 2
+    Exit: 3
+    """))
+    if intial == 3:
+        break
+
