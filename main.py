6728319c24fadea55a1f98480a7dd57da36600a6
Alex W <alexcchip@gmail.com>
2023-06-04 19:12:22 -0400

    WORKING file stuff

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
 import config
+import os
 import yfinance as yf
-f = open("list.txt", "w+")
-stock = yf.Ticker("MSFT")
+
+#stock = yf.Ticker("MSFT")
 
 # get all stock info
-price = stock.info['currentPrice']
-print(price)
+#price = stock.info['currentPrice']
+#print(price)
 
 print("     Welcome to your personalized stock watchlist")
-while True:
-    initial = int(input("""Add: 1
-    Read: 2
-    Edit: 3
-    Exit: 4
-    """))
-    if initial == 1:
-        fileName= input("Name of watchlist: ")
-        f = open(f"{fileName}", "w+")
-        num = int(input("How many stocks to watch? "))
-        for i in range (num):
-            sAdd = input("Ticker name: ")
-            f.write(sAdd+",")
+
+initial = int(input("""
+Add: 1
+Delete: 2
+"""))
+if initial == 1:
+    fileName= input("Name of watchlist: ")
+    f = open(f"{fileName}", "w+")
+    num = int(input("How many stocks to watch? "))
+    for i in range (num):
+        sAdd = input("Ticker name: ")
+        f.write(sAdd+",")
+elif initial == 2:
+    toDelete = input("File name to delete: ")
+    os.remove(f"{toDelete}")
 
 
 #https://stackoverflow.com/questions/61104362/how-to-get-actual-stock-prices-with-yfinance
