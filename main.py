70dd10a8779aa36bad9afd5f42ef3144988e9a53
Alex W <alexcchip@gmail.com>
2023-06-04 17:56:57 -0400

    Config + main

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
+import conflig
+
 print("     Welcome to your personalized stock watchlist")
 while True:
-    intial = int(input("""Read: 1)
-    Edit: 2
-    Exit: 3
+    initial = int(input("""Add: 1
+    Read: 2
+    Edit: 3
+    Exit: 4
     """))
-    if intial == 3:
+    if initial == 1:
+        listName= input("Name of watchlist: ")
+        listName = []
+        num = int(input("How many stocks to watch? "))
+        for i in range (num):
+            sAdd = input("Ticker name: ")
+            listName.append(sAdd)
+    elif initial == 2:
+        pass
+    elif initial == 3:
+        pass
+    elif initial == 4:
         break
+
+
 
