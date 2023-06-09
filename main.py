2b2888c7f9568cb959c2343cfdc714442df5dc21
Alex W <alexcchip@gmail.com>
2023-06-09 02:49:28 -0400

    Something new

--- main.py
+++ main.py
@@ -6,16 +6,23 @@ save_path = "Watchlists"
 # get all stock info
 #price = stock.info['currentPrice']
 #print(price)
-running = True
-watchlists = {"Tech": ['AAPL','MSFT','NVDA','PLTR','AMD','TSM','TSLA'],'Bank/Finance': ['JPM','BAC','MS','GS','C','RY','SCHW'],'Healthcare':['PFE','AMGN','JNJ','LLY','GILD','AZN','MRNA']}
-def getData():
-    start = input("Which watchlist would you like to read from (Tech, Bank/Finance or Healthcare)? Hit enter to exit program.")
-    if start == "":
-        running = False
+watchlists = {"Tech": ['AAPL','MSFT','NVDA','PLTR','AMD','TSM','TSLA'],'Finance': ['JPM','BAC','MS','GS','C','RY','SCHW'],'Healthcare':['PFE','AMGN','JNJ','LLY','GILD','AZN','MRNA']}
+
+
+def gettingData(choice):
+    stock = yf.Ticker(choice)
+    price = stock.info['currentPrice']
+    print("Last Price:",price)
+
+
+while True:
+    start = input("Which watchlist would you like to read from (Tech, Finance or Healthcare)? ")
+    print("\nPlease choose a stock to read from:")
     for i in watchlists[start]:
         print(i)
-    
-
-while running:
-    getData()
+    choiceOfStock = input("\n")
+    gettingData(choiceOfStock)
+    running = input("\nWould you like to read more data (y/n)? ")
+    if running == "n":
+        break
 #https://stackoverflow.com/questions/61104362/how-to-get-actual-stock-prices-with-yfinance
