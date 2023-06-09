ff711d83007a3cd89634c45b79496a8cd5e24569
Alex Weinberger <weinberger.al@northeastern.edu>
2023-06-09 16:27:28 -0400

    reset

--- main.py
+++ main.py
@@ -1,53 +1,46 @@
 import os
 import yfinance as yf
-save_path = "Watchlists"
+#save_path = "Watchlists"
 #stock = yf.Ticker("MSFT")
 
 # get all stock info
 #price = stock.info['currentPrice']
 #print(price)
-
+WLS = dict()
 def readWLs():
-    for i in os.listdir(save_path):
+    for i in WLS:
             print("     "+i+"\n")
+    print(WLS)
 def readStocks():
      for i in f.readlines():
+
+        stock = yf.Ticker(i)
+        price = stock.info.get('currentPrice')  
         print(i)
-     f.close()
+        print(price)
 
 print("     Welcome to your personalized stock watchlist")
 while True:
-    initial = int(input("""
-    Add: 1
-    Delete: 2
-    Read: 3
-    Edit: 4
-    Exit: 5
+    initial = int(input("""\n 
+    Add: 1 | Delete: 2 | Read: 3 | Edit: 4 | Exit: 5
     """))
     if initial == 1:
-        fileName= input("Name of watchlist: ")
-        directedFile =  os.path.join(save_path, fileName)
-        f = open(f"{directedFile}", "a")
+        wlName= input("Name of watchlist: ")
+        grouping = []
         num = int(input("How many stocks to watch? "))
         for i in range(num):
             sAdd = input("Ticker name: ")
-            f.write(str(i)+": "+sAdd)
-            f.write("\n")
-        f.close()
+            grouping.append(sAdd)
+        WLS[wlName] = grouping
     elif initial == 2:
-        toDelete = input("File name to delete: ")
-        os.remove(f"Watchlists"+"\\"+toDelete)
+        toDelete = input("Watchlist to delete: ")
+        WLS.pop(toDelete)
     elif initial == 3:
         print("Which watchlist would you like to read from? ")
         readWLs()
         readingWL = input("\n")
-        f = open(save_path+"\\"+readingWL)
+        #f = open(save_path+"\\"+readingWL)
         readStocks()
-        print("Which stock would you like to see the price of? ")
-        stockReadingChoice = input("\n")
-        stock = yf.Ticker(stockReadingChoice)
-        price = stock.info.get('currentPrice')
-        print(price)
     elif initial == 4:
         print("Which watchlist would you like to edit? ")
         readWLs()
