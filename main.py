5c8f4b64114d15ee998f446e6b3e3fa5ac2eee54
Alex W <alexcchip@gmail.com>
2023-06-09 02:32:45 -0400

    test

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
+import config
 import os
 import yfinance as yf
-save_path = "Watchlists"
+save_path = "M:\Coding\Python\Stock-Watchlist\Watchlists"
 #stock = yf.Ticker("MSFT")
 
 # get all stock info
 #price = stock.info['currentPrice']
 #print(price)
-
-def readWLs():
-    for i in os.listdir(save_path):
-            print("     "+i+"\n")
-def readStocks():
-     for i in f.readlines():
-        print(i)
-     f.close()
 
 print("     Welcome to your personalized stock watchlist")
 while True:
@@ -21,37 +14,34 @@ while True:
     Add: 1
     Delete: 2
     Read: 3
-    Edit: 4
-    Exit: 5
+    Exit: 4
     """))
     if initial == 1:
-        fileName= input("Name of watchlist: ")
+        wlName= input("Name of watchlist: ")
         directedFile =  os.path.join(save_path, fileName)
         f = open(f"{directedFile}", "a")
         num = int(input("How many stocks to watch? "))
-        for i in range(num):
+        for i in range (num):
             sAdd = input("Ticker name: ")
-            f.write(str(i)+": "+sAdd)
+            f.write(sAdd)
             f.write("\n")
         f.close()
     elif initial == 2:
         toDelete = input("File name to delete: ")
-        os.remove(f"Watchlists"+"\\"+toDelete)
+        os.remove(f"M:\Coding\Python\Stock-Watchlist\Watchlists"+"\\"+toDelete)
     elif initial == 3:
         print("Which watchlist would you like to read from? ")
-        readWLs()
+        for i in os.listdir(save_path):
+            print("     "+i+"\n")
         readingWL = input("\n")
         f = open(save_path+"\\"+readingWL)
-        readStocks()
+        for i in f.readlines():
+            print(i)
         print("Which stock would you like to see the price of? ")
         stockReadingChoice = input("\n")
         stock = yf.Ticker(stockReadingChoice)
         price = stock.info.get('currentPrice')
         print(price)
     elif initial == 4:
-        print("Which watchlist would you like to edit? ")
-        readWLs()
-        wlEdit = input()
-    elif initial == 5:
         break
 #https://stackoverflow.com/questions/61104362/how-to-get-actual-stock-prices-with-yfinance
